15d088ffb26adc03fe0c5bbc6f0f9df292618921
ramasantayana <rama.santayana@uconn.edu>
2023-05-05 21:52:18 -0400

    get routes done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,24 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+    .then((dbCategoryData) => res(dbCategoryData))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
