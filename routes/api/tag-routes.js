57574dab8b68bf83c1899282a39e3878a101116d
ramasantayana <rama.santayana@uconn.edu>
2023-05-31 06:45:39 -0400

    updated tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,6 +24,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      }
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
